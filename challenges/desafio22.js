f3e2fb7e42c73392e863a2120a607022afd52ef7
Rodrigo Lima <limarodrigoo@outlook.com>
2022-05-16 17:09:00 -0300

    fix: errors lint

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -12,9 +12,10 @@ db.produtos
       $inc: {
         "vendasPorDia.3": 60,
       },
-    });
+    }
+  );
 db.produtos
   .updateMany(
     { tags: { $in: ["bovino"] } },
-    { $inc: { "vendasPorDia.6": 120, }, });
+    { $inc: { "vendasPorDia.6": 120 } });
 db.produtos.find({}, { nome: true, vendasPorDia: true, _id: false });
