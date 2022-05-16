1113a761fbaea2c4eabffbe08858dbebe6ab9ba7
Rodrigo Lima <limarodrigoo@outlook.com>
2022-05-16 15:39:43 -0300

    ✅ desafio16

---
+++ challenges/desafio16.js
@@ -0,0 +1,13 @@
+let todayDate = new Date();
+db.produtos
+  .updateOne({ nome: 'Big Mac' },
+    {
+      $set: {
+        ultimaModificacao: todayDate
+      }
+    });
+db.produtos.find({
+  ultimaModificacao:
+    { $exists: true }
+},
+  { nome: true, _id: false });
