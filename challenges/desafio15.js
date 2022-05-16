075de1679d5b067d3a8038bced913be8a04a11c8
Rodrigo Lima <limarodrigoo@outlook.com>
2022-05-16 15:39:37 -0300

    ✅ desafio15

---
+++ challenges/desafio15.js
@@ -0,0 +1,24 @@
+db.produtos
+  .updateMany({},
+    {
+      $set: {
+        avaliacao: 0,
+      }
+    });
+db.produtos.updateMany(
+  { tags: 'bovino' },
+  {
+    $set: {
+      avaliacao: 5,
+    }
+  }
+);
+db.produtos.updateMany(
+  { tags: 'ave' },
+  {
+    $set: {
+      avaliacao: 3,
+    }
+  }
+);
+db.produtos.find({}, { nome: true, avaliacao: true, _id: false });
